2bff3117540645d73ed4d1250366f3473d39d3f5
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2021-08-26 17:38:20 +0100

    :recycle: [REFACTOR]: Clean code base

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 import { errorCreator, errorHandler } from './helpers/error-handler';
-import { getAllBanks, getBankByName } from './controllers/banks.controllers';
+import { getAllBanks, getBanksByName } from './controllers/banks.controllers';
 import path from 'path';
 
 const app: Express = express();
@@ -15,7 +15,7 @@ console.log(__dirname);
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 app.get('/banks', getAllBanks);
-app.get('/banks/:filter', getBankByName);
+app.get('/banks/:filter', getBanksByName);
 
 // create error
 app.use(errorCreator);
